04dcc0f2ba634ae81222bf95c40b2fc677d6feaf
mariane <mariane.maynard@usherbrooke.ca>
2017-07-29 12:25:35 -0400

    Mybot version 2

--- MyBot/MyBot.py
+++ MyBot/MyBot.py
@@ -4,17 +4,32 @@ import random
 
 
 myID, game_map = hlt.get_init()
-hlt.send_init("TofuBot")
+hlt.send_init("TofuBot-Mariane")
 
 def assign_move(square):
-    for direction, neighbor in enumerate(game_map.neighbors(square)):
-        if neighbor.owner != myID and neighbor.strength < square.strength:
-            return Move(square, direction)
-        
-    if square.strength < 5 * square.production:
+    directions = {NORTH, WEST, SOUTH, EAST}
+    areAllNeighborsMe = True
+    neighbors = enumerate(game_map.neighbors(square))
+    numbOfNeighbors = 0
+
+    for direction, neighbor in neighbors:
+        directions.remove(direction)
+        numbOfNeighbors += 1
+        if neighbor.owner != myID:
+            areAllNeighborsMe = False
+            #capture du voisin s'il est plus faible que nous
+            if neighbor.strength < square.strength:
+                return Move(square, direction)
+    
+    #pièce centrale - on tente de mettre la force sur les cotes
+    if areAllNeighborsMe and numbOfNeighbors == 4 and square.strength > 0:
+        return Move(square, random.choice((NORTH, SOUTH, EAST, WEST)))
+    #si notre piece n'est assez forte on reste
+    if square.strength < min(2 * square.production,128) or len(directions) == 0:
         return Move(square, STILL)
+    #si notre piece est assez forte on la bouge dans les directions qui ne sont pas encore prises
     else:
-        return Move(square, random.choice((NORTH, WEST)))
+        return Move(square, random.choice(tuple(directions)))
 
 
 while True:
