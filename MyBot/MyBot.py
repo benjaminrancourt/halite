502e82a0a560e3ff5d9da52e41bed4f27bb2464e
Benjamin Rancourt <benjamin.rancourt@usherbrooke.ca>
2017-07-29 13:00:04 -0400

    Map informations, random intercardinal point and comments

--- MyBot/MyBot.py
+++ MyBot/MyBot.py
@@ -1,13 +1,37 @@
 import hlt
 from hlt import NORTH, EAST, SOUTH, WEST, STILL, Move, Square
 import random
-
+import math
 
 myID, game_map = hlt.get_init()
-hlt.send_init("TofuBot-Mariane")
+
+# Hauteur de la carte
+heigth = game_map.height
+
+# Largeur de la carte
+witdh = game_map.width
+
+# Nombre de tours
+numTurns = int(10 * math.sqrt(heigth * witdh))
+
+# Tour courant
+currentTurn = 0
+
+# Points intercardinaux
+interCardinalDirections = [
+    (NORTH, EAST),
+    (SOUTH, EAST),
+    (SOUTH, WEST),
+    (NORTH, WEST),
+]
+
+# Point intercardinal aléatoire
+randomInterCardinalDirection = random.choice(interCardinalDirections)
+
+hlt.send_init("TofuBot-Benjamin")
 
 def assign_move(square):
-    directions = {NORTH, WEST, SOUTH, EAST}
+    directions = {NORTH, WEST, SOUTH, EAST, STILL}
     areAllNeighborsMe = True
     neighbors = enumerate(game_map.neighbors(square))
     numbOfNeighbors = 0
@@ -15,21 +39,27 @@ def assign_move(square):
     for direction, neighbor in neighbors:
         directions.remove(direction)
         numbOfNeighbors += 1
+
         if neighbor.owner != myID:
             areAllNeighborsMe = False
-            #capture du voisin s'il est plus faible que nous
+
+            # Capture du voisin s'il est plus faible que nous
             if neighbor.strength < square.strength:
                 return Move(square, direction)
     
-    #pièce centrale - on tente de mettre la force sur les cotes
-    if areAllNeighborsMe and numbOfNeighbors == 4 and square.strength > 0:
-        return Move(square, random.choice((NORTH, SOUTH, EAST, WEST)))
-    #si notre piece n'est assez forte on reste
-    if square.strength < min(2 * square.production,128) or len(directions) == 0:
+    # Pièce entourée de pièces amies
+    if areAllNeighborsMe and numbOfNeighbors == 4:
+        if square.strength < 32:
+            return Move(square, STILL)
+
+        return Move(square, random.choice(randomInterCardinalDirection))
+
+    # Si notre piece n'est pas assez forte, on reste
+    if square.strength < max(2 * square.production, 128) or len(directions) == 0:
         return Move(square, STILL)
-    #si notre piece est assez forte on la bouge dans les directions qui ne sont pas encore prises
-    else:
-        return Move(square, random.choice(tuple(directions)))
+
+    # Si notre piece est assez forte, on la bouge dans les directions qui ne sont pas encore prises
+    return Move(square, random.choice(tuple(directions)))
 
 
 while True:
@@ -39,3 +69,4 @@ while True:
     # moves = [Move(square, random.choice((NORTH, EAST, SOUTH, WEST, STILL))) for square in game_map if square.owner == myID]
     moves = [assign_move(square) for square in game_map if square.owner == myID]
     hlt.send_frame(moves)
+    currentTurn += 1
