6936b61c02f3462e5cbdd0700c259fc489b5b58e
Benjamin Rancourt <benjamin.rancourt@usherbrooke.ca>
2017-07-29 09:52:20 -0400

    The server need MyBot.py file

--- runGame.bat
+++ runGame.bat
@@ -1,1 +1,1 @@
-.\halite\halite.exe -d "30 30" "python TofuBot\TofuBot.py" "python RandomBot\RandomBot.py" "python RandomBot\RandomBot.py" "python RandomBot\RandomBot.py"
+.\halite\halite.exe -d "30 30" "python MyBot\MyBot.py" "python RandomBot\RandomBot.py" "python RandomBot\RandomBot.py" "python RandomBot\RandomBot.py"
--- runGame.sh
+++ runGame.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if hash python3 2>/dev/null; then
-    ./halite/halite -d "30 30" "python3 TofuBot/TofuBot.py" "python3 RandomBot/RandomBot.py" "python3 RandomBot/RandomBot.py" "python3 RandomBot/RandomBot.py"
+    ./halite/halite -d "30 30" "python3 MyBot/MyBot.py" "python3 RandomBot/RandomBot.py" "python3 RandomBot/RandomBot.py" "python3 RandomBot/RandomBot.py"
 else
-    ./halite/halite -d "30 30" "python TofuBot/TofuBot.py" "python RandomBot/RandomBot.py" "python RandomBot/RandomBot.py" "python RandomBot/RandomBot.py"
+    ./halite/halite -d "30 30" "python MyBot/MyBot.py" "python RandomBot/RandomBot.py" "python RandomBot/RandomBot.py" "python RandomBot/RandomBot.py"
 fi
